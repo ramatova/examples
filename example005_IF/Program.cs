96243686e9b267d89837bc4b377007ba1e0259dd
Ramatova <aamaratovna@mail.ru>
2023-07-11 22:18:54 +0600

    add Example 005 IF

---
+++ example005_IF/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "emil")
+{
+	Console.WriteLine("i love you, Emil");
+}
+else 
+{
+	Console.Write("Privet");
+	Console.WriteLine(username);
+}
